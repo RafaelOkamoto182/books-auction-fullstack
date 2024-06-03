bdb8154ee195b167f3746aa5d8046389666eaa9d
Rafael Okamoto <rafa.mg1@hotmail.com>
2024-06-03 13:52:45 -0300

    backend-fix/remove users resource

--- backend/src/sellers/sellers.controller.ts
+++ backend/src/sellers/sellers.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { SellersService } from './sellers.service';
 import { CreateSellerDto } from './dto/create-seller.dto';
 import { UpdateSellerDto } from './dto/update-seller.dto';
-import { Sellers } from './sellers.interface';
 
 @Controller('sellers')
 export class SellersController {
